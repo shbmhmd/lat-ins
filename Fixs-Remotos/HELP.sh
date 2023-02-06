3a6d078319778ab6b8f2c24bf954a143450ee1ed
LaCasitaMx <69657855+lacasitamx@users.noreply.github.com>
2023-02-05 19:08:50 -0600

    Add files via upload

---
+++ Fixs-Remotos/HELP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Soporte Remoto (HELP)
+echo "" >/dev/null 2>&1
+wget raw.github.com/lacasitamx/SCRIPT-LATAM/Netvps/Fixs-Remotos/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
--- Fixs-Remotos/SPR.sh
+++ Fixs-Remotos/SPR.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+#Soporte Remoto (SPR)
+echo "" >/dev/null 2>&1
+wget raw.github.com/lacasitamx/SCRIPT-LATAM/Netvps/Fixs-Remotos/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
